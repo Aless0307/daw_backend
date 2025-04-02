bcae520275749b4ba0c7b59f442b9d6d6a6cf6a9
Alessandro Atilano <alessandroah77@gmail.com>
2025-04-02 12:53:35 -0600

    intento 3, corrigiendo el import desde keys

--- Database/neo4j_conn.py
+++ Database/neo4j_conn.py
@@ -2,9 +2,9 @@ from neo4j import GraphDatabase
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) # para agregar la carpeta padre al path y poder importar las keys
-from keys import URI, USER, PASSWORD #Importo las credenciales de conexión desde mi archivo que contiene las keys
+from keys import NEO4J_URI, NEO4J_USER, NEO4J_PASSWORD #Importo las credenciales de conexión desde mi archivo que contiene las keys
 
-driver = GraphDatabase.driver(URI, auth=(USER, PASSWORD))
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 def get_neo4j_session():
     return driver.session()
